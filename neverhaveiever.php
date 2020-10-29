a126eb4d8470a13ec647e942e9899c7b38d661ad
Thomas Kulmbach <thomas@dentalmedia.dk>
2020-10-29 14:30:14 +0100

    two ned questions

--- neverhaveiever.php
+++ neverhaveiever.php
@@ -8,4 +8,6 @@
         'experimented to see my sexual orientation.',
         'said ‘I love you’ just to get laid',
         'spied on my neighbors',
+        'Lied to a friend to avoid a greater evil',
+        'Done a handstand with one hand',
     ];
